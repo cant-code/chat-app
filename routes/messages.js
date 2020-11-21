6f6948870fc16f40069926e6b6addd9f9b854cc8
Ayush <ayushc13000@gmail.com>
2020-11-21 21:26:39 +0530

    router and cra boilerplate

---
+++ routes/messages.js
@@ -0,0 +1,197 @@
+const express = require('express');
+const router = express.Router();
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+
+const key = process.env.SECRET;
+const verify = require('../utils/verifyToken');
+const Message = require('../models/Message');
+const Convo = require('../models/Conversation');
+const Global = require('../models/GlobalMessage');
+
+let jwtUser = null;
+
+router.use(function(req, res, next) {
+    try {
+        jwtUser = jwt.verify(verify(req), key);
+        next();
+    } catch(e) {
+        console.log(e);
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({ Error: 'Unauthorized'}));
+        res.sendStatus(401);
+    }
+});
+
+router.get('/global', (req, res) => {
+    Global.aggregate([
+        {
+            $lookup: {
+                from: 'users',
+                localField: 'from',
+                foreignField: '_id',
+                as: 'fromObj',
+            },
+        },
+    ])
+    .project({
+        'fromObj.password': 0,
+        'fromObj.date': 0
+    })
+    .exec((e, msgs) => {
+        if(e) {
+            console.log(e);
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify({ error: 'Error'}));
+            res.sendStatus(500);
+        } else res.send(msgs);
+    });
+});
+
+router.post('/global', (req, res) => {
+    let msg = new Global({
+        from: jwtUser.id,
+        body: req.body.data,
+    });
+
+    req.io.sockets.emit('messages', req.body.data);
+
+    msg.save(err => {
+        if(err) {
+            console.log(err);
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify({ error: 'Error' }));
+            res.sendStatus(500);
+        } else {
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify({ message: 'Success'}));
+        }
+    });
+});
+
+router.get('/convo', (req, res) => {
+    let from = mongoose.Types.ObjectId(jwtUser.id);
+    Convo.aggregate([
+        {
+            $lookup: {
+                from: 'users',
+                localField: 'recipients',
+                foreignField: '_id',
+                as: 'recipientObj',
+            },
+        },
+    ])
+    .match({ recipients: { $all: [{ $elemMatch: { $eq: from }}]}})
+    .project({
+        'recipientObj.password': 0,
+        'recipientObj.date': 0,
+    })
+    .exec((err, convos) => {
+        if(err) {
+            console.log(err);
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify({ message: 'Failure' }));
+            res.sendStatus(500);
+        } else res.send(convos);
+    });
+});
+
+router.get('/convos/query', (req,res) => {
+    let user1 = mongoose.Types.ObjectId(jwtUser.id);
+    let user2 = mongoose.Types.ObjectId(req.query.userId);
+    Message.aggregate([
+        {
+            $lookup: {
+                from: 'users',
+                localField: 'to',
+                foreignField: '_id',
+                as: 'toObj',
+            },
+        },
+        {
+            $lookup: {
+                from: 'users',
+                localField: 'from',
+                foreignField: '_id',
+                as: 'fromObj',
+            },
+        },
+    ])
+    .match({
+        $or: [
+            { $and: [{ to: user1}, { from: user2}]},
+            { $and: [{ to: user2}, { from: user1}]},
+        ],
+    })
+    .project({
+        'toObj.password': 0,
+        'toObj.date': 0,
+        'fromObj.password': 0,
+        'fromObj.date': 0
+    })
+    .exec((err, msgs) => {
+        if(err) {
+            console.log(err);
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify({ message: 'Failure' }));
+            res.sendStatus(500);
+        } else res.send(msgs);
+    });
+});
+
+router.post('/', (req,res) => {
+    let from = mongoose.Types.ObjectId(jwtUser.id);
+    let to = mongoose.Types.ObjectId(req.body.to);
+
+    Convo.findOneAndUpdate(
+    {
+        recipients: {
+            $all: [
+                { $elemMatch: { $eq: from }},
+                { $elemMatch: { $eq: to }},
+            ],
+        },
+    },
+    {
+        recipients: [jwtUser.id, req.body.to],
+        lastMessage: req.body.data,
+        date: Date.now(),
+    },
+    { upsert: true, new: true, setDefaultsOnInsert: true },
+    function(err, conversation) {
+        if(err) {
+            console.log(err);
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify({ message: 'Failure' }));
+            res.sendStatus(500);
+        } else {
+            let message = new Message({
+                conversation: conversation._id,
+                to: req.body.to,
+                from: jwtUser.id,
+                body: req.body.data,
+            });
+
+            req.io.sockets.emit('messages', req.body.body);
+
+            message.save(err => {
+                if (err) {
+                    console.log(err);
+                    res.setHeader('Content-Type', 'application/json');
+                    res.end(JSON.stringify({ message: 'Failure' }));
+                    res.sendStatus(500);
+                } else {
+                    res.setHeader('Content-Type', 'application/json');
+                    res.end(
+                        JSON.stringify({
+                            message: 'Success',
+                            conversationId: conversation._id,
+                        })
+                    );
+                }
+            });
+        }
+    });
+});
+
+module.exports = router;
---
+++ routes/users.js
@@ -0,0 +1,122 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+
+const key = process.env.SECRET;
+const verify = require('../utils/verifyToken');
+const validateRegistration = require('../utils/register');
+const validateLogin = require('../utils/login');
+const User = require('../models/User');
+
+router.get('/', (req, res) => {
+    try {
+        let jwtUser = jwt.verify(verify(req), key);
+        let id = mongoose.Types.ObjectId(jwtUser.id);
+
+        User.aggregate()
+        .match({ _id: { $not: { $eq: id }}})
+        .project({
+            password: 0,
+            date: 0,
+        })
+        .exec((e, user) => {
+            if(e) {
+                console.log(e);
+                res.setHeader('Content-Type', 'application/json');
+                res.end(JSON.stringify({ Error: 'error'}));
+                res.sendStatus(500);
+            }
+            else res.send(user);
+        });
+    }
+    catch (e) {
+        console.log(e);
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({ Error: 'Unauthorized'}));
+        res.sendStatus(401);
+    }
+});
+
+router.post('/register', (req, res) => {
+    const { errors, isValid } = validateRegistration(req.body);
+    if (!isValid) return res.status(400).json(errors);
+    User.findOne({ username: req.body.username }).then(user => {
+        if(user) return res.status(400).json({ error: 'User already exists'});
+        else {
+            const newUser = new User({
+                name: req.body.name,
+                username: req.body.username,
+                password: req.body.password,
+            });
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, salt) => {
+                    if(err) throw err;
+                    newUser.password = hash;
+                    newUser.save().then(user => {
+                        const payload = {
+                            id: user.id,
+                            name: user.name
+                        };
+                        jwt.sign(
+                            payload,
+                            key,
+                            {
+                                expiresIn: 86400,
+                            },
+                            (e, token) => {
+                                if(e) throw e;
+                                else {
+                                    req.io.sockets.emit('users', user.username);
+                                    res.json({
+                                        success: true,
+                                        token: 'Bearer ' + token,
+                                        name: user.name
+                                    });
+                                }
+                            }
+                        );
+                    }).catch(e => console.log(e));
+                });
+            });
+        }
+    });
+});
+
+router.post('/login', (req, res) => {
+    const { errors, isValid } = validateLogin(req.body);
+    if(!isValid) return res.status(400).json(errors);
+    const { username, password } = req.body;
+    User.findOne({ username }).then(user => {
+        if(!user) return res.status(404).json({ Error: 'User not found'});
+        bcrypt.compare(password, user.password).then(found => {
+            if(found) {
+                const payload = {
+                    id: user.id,
+                    name: user.name
+                };
+                jwt.sign(
+                    payload,
+                    key,
+                    {
+                        expiresIn: 86400,
+                    },
+                    (e, token) => {
+                        res.json({
+                            success: true,
+                            token: 'Bearer ' + token,
+                            name: user.name,
+                            username: user.username,
+                            userId: user._id,
+                        });
+                    }
+                );
+            } else {
+                return res.status(400).json({ Error: 'Credentials do not match'});
+            }
+        });
+    });
+});
+
+module.exports = router;
